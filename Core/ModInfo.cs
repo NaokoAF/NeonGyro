a8cc629cb44b70cc4118de59d3e98988b494c25b
Nao <30605154+NaokoAF@users.noreply.github.com>
2025-04-24 08:35:40 -0300

    Change version to 1.1.0

--- Core/ModInfo.cs
+++ Core/ModInfo.cs
@@ -5,5 +5,5 @@ public static class ModInfo
 	public const string Guid = "NaokoAF.NeonGyro";
 	public const string Name = "NeonGyro";
 	public const string Author = "NaokoAF";
-	public const string Version = "1.0.0";
+	public const string Version = "1.1.0";
 }
